df22280ceb563b639b188c13f75514074d8b99b9
Avani-2019 <72139667+Avani-2019@users.noreply.github.com>
2021-07-19 06:16:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bg,bg1,form,system,code,security;
+var score=0;
+
+function preload() {
+ 
+  
+  bg= loadImage("aladdin_cave.jpg")
+  //load image for the treasure background
+  bg1= loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1000,500);
+  security = new Security();
+  system = new System();
+  
+  
+  
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+// add code for changing the background to the treasure background
+  if(score === 3) {
+    clear()
+    background(bg1)
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+  }
+
+  drawSprites()
+}
